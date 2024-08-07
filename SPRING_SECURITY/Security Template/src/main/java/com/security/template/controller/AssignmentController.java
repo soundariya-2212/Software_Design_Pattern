946192c5237a4046810b75998371215bd4b38fbd
soundariya-2212 <151667083+soundariya-2212@users.noreply.github.com>
2024-08-07 14:23:11 +0530

    Delete SPRING_SECURITY/Security Template directory

--- SPRING_SECURITY/Security Template/src/main/java/com/security/template/controller/AssignmentController.java
+++
@@ -1,5 +0,0 @@
-package com.security.template.controller;
-
-public class AssignmentController {
-
-}
--- SPRING_SECURITY/Security Template/src/main/java/com/security/template/repo/AssignmentRepo.java
+++
@@ -1,9 +0,0 @@
-package com.security.template.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.security.template.model.Assignment;
-
-public interface AssignmentRepo extends JpaRepository<Assignment, Long> {
-
-}
