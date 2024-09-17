02c54d76874e263a9fb62d92d0fd787a503d46ec
soundariya-2212 <151667083+soundariya-2212@users.noreply.github.com>
2024-09-17 14:42:55 +0530

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,41 @@
 
-import path from "path"
+// import path from "path"
 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react-swc'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   plugins: [react()],
+//   build: {
+//     rollupOptions: {
+//       external: ['react-toastify'],
+//     },
+//   },
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+// })
+
+
+import path from "path";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['react-toastify'],
+    },
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
